a7c7a220f4ad59d28b1f6feeadd41358eccc6b14
Tijn Somers <tijnsomers@telenet.be>
2024-08-02 22:10:07 +0200

    Footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <header>
     <nav>
         <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="/links">links</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/links">Links</a></li>
         </ul>
     </nav>
 </header>
 
 <slot></slot>
+
+<footer>
+    <p>© Tino_Zwino 2024</p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,2 @@
-<script>
-    import Header from "./Header.svelte";
-    import Footer from "./footer.svelte";
-</script>
 
-<Header></Header>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<Footer></Footer>
+<h1>Welcome</h1>
